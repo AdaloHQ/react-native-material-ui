74af8b7c36d0fa86a40561360b019946e25952f4
Daniel Costa <danielcc.jp@gmail.com>
2023-03-28 10:37:52 -0300

    fix: import viewproptypes from deprecated module (removed from RN)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,9 @@
-import {
-  View,
-  BackHandler,
-  ViewPropTypes as RNViewPropTypes,
-} from 'react-native';
+import { View, BackHandler } from 'react-native';
+
+import { ViewPropTypes as RNViewPropTypes } from 'deprecated-react-native-prop-types';
 
 const ViewPropTypes = RNViewPropTypes || View.propTypes; // eslint-disable-line
+
 const BackAndroid = BackHandler; // BackAndroid is not supported anymore in react-native.
 
 export { ViewPropTypes, BackAndroid };
